04cbb0371af26950a4b059b1344c39d9742a3095
Clifford Dalson III <cdalsoniii@gmail.com>
2023-06-18 00:51:14 -0400

    remove secrets

--- lib/backend/supabase/supabase.dart
+++ lib/backend/supabase/supabase.dart
@@ -2,9 +2,8 @@ import 'package:supabase_flutter/supabase_flutter.dart' hide Provider;
 
 export 'database/database.dart';
 
-const _kSupabaseUrl = 'https://yndxsdhglaxljiefbsjy.supabase.co';
-const _kSupabaseAnonKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InluZHhzZGhnbGF4bGppZWZic2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY5NDY0NjQsImV4cCI6MjAwMjUyMjQ2NH0.su4me-LNeytTEhtIr7q06HjsA4L4S17PvGf42nMagYw';
+const _kSupabaseUrl = '';
+const _kSupabaseAnonKey = '';
 final supabase = Supabase.instance.client;
 
 class SupaFlow {
